f3b838c9b371f33de5d5cc44367e5cc529a9135f
HunterH0543 <145065798+HunterH0543@users.noreply.github.com>
2024-03-22 22:05:22 +0000

    Fri, Mar 22, 2024, 3:05 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -142,6 +142,23 @@ document.addEventListener('keyup', keyUp)
 function moveBall() {
     ball.x = ball.x + ball.dx
     ball.y = ball.y + ball.dy
+
+    //Wall Collision(top)
+    if (ball.y + ball.size < 0) {
+        ball.dy = -1 * ball.dy
+    }
+    //Wall Collision(right)
+    if (ball.x + ball.size > canvas.width){
+        ball.dx = -1 * ball.dx
+    }
+    //Wall Collision(bottom)
+    if (ball.y + ball.size > canvas.height) {
+        ball.dy = -1 * ball.dy
+    }
+    // Wall Collision(left)
+    if(ball.x + ball.size < 0){
+        ball.dx = -1 * ball.dx
+    }
 }
 //Update the Canvas Drawing and do the Animation
 function update () {
