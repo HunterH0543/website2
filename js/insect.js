123b5f1e38592ffba2ae21e29ee680b66298f3ee
HunterH0543 <145065798+HunterH0543@users.noreply.github.com>
2024-04-18 21:35:18 +0000

    Thu, Apr 18, 2024, 2:35 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,1 +1,14 @@
 const screens = document.querySelectorAll('.screen')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getELementById('time')
+const scoreEl = document.getELementById('score')
+const message = document.getELementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
+
+start_btn.addEventListener('click', () => {
+    screens[0].claddList.add('up')
+})
