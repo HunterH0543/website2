3bd598db7492c491828b3b64a6490d2995210d2c
HunterH0543 <145065798+HunterH0543@users.noreply.github.com>
2024-04-23 21:33:46 +0000

    Tue, Apr 23, 2024, 2:33 PM -07:00

---
+++ js/hangman.js
@@ -0,0 +1,12 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementByid('wrong-letters')
+const playAgainBtn = document.getElementByID('play-again')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById ('notification-container')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['application', 'programming', 'interface', 'wizard']
+
+let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = word[SelectedIndex]
