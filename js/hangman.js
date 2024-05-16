a305964dee44d7ac14bd2430918630b0711e1acf
HunterH0543 <145065798+HunterH0543@users.noreply.github.com>
2024-05-16 16:07:09 -0700

    fixed hangman

--- js/hangman.js
+++ js/hangman.js
@@ -2,11 +2,11 @@ const wordEl = document.getElementById('word')
 const wrongLettersEl = document.getElementById('wrong-letters')
 const playAgainBtn = document.getElementById('play-button')
 const popup = document.getElementById('popup-container')
-const notification = document.getElementById('notification-center')
+const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
 
-const word = ['application', 'programming', 'interface', 'wizard']
+const word = ['application', 'programming', 'interface', 'wizard', 'apple', 'orange', 'lemon', 'water', 'computer', 'mobile', 'vehicle']
 
 let selectedIndex = Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
@@ -14,18 +14,20 @@ let selectedWord = word[selectedIndex]
 const correctLetters = []
 const wrongLetters = []
 
-// Show Hidden Word
+// Show hidden word
 function displayWord() {
     wordEl.innerHTML = `
-        ${selectedWord
+    ${selectedWord
         .split('')
         .map(letter => `
-        <span class="letters">
-            ${correctLetters.includes(letter) ? letter : ''}
-        <span>
-        `).join('')
-        }
-    `
+            <span class="letter">
+                ${correctLetters.includes(letter) ? letter : ''}
+            </span>
+        ` ).join('')
+    }
+        `
+
+
     const innerWord = wordEl.innerText.replace(/\n/g, '')
 
     if (innerWord == selectedWord) {
@@ -36,14 +38,14 @@ function displayWord() {
 
 
 
-// Update the wrong letters
-function updateWrongLetters() {
-    // Display Wrong Letters
+// Update wrong letters
+function updateWrongLettersEl () {
     wrongLettersEl.innerHTML = `
-        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
-        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
     `
-    // Display Parts
+
+    // Draw figure
     figureParts.forEach((part, index) => {
         const errors = wrongLetters.length
 
@@ -55,53 +57,60 @@ function updateWrongLetters() {
         }
     })
 
-    // Check if Lost
-    If (wrongLetters.length == figureParts.length)
+    // Check if lost
+    if (wrongLetters.length == 6) {
         finalMessage.innerText = 'Unfortunately you lost!'
         popup.style.display = 'flex'
+    }
 }
 
-// Show Notification
+
+// Show notification
 function showNotification() {
     notification.classList.add('show')
+
 
     setTimeout(() => {
         notification.classList.remove('show')
     }, 2000)
 }
 
-//Keydown letter press
+
+
+// Keydown letter press
 window.addEventListener('keydown', e => {
-
-    if (e.keyCode >= 65 && e.keyCode <= 90) {
+    if (e.keyCode >= 65 && e.keyCode <=90) {
         const letter = e.key
-    }
-    if (selectedWord.includes(letter)) {
-        if( !correctLetters.includes(letter)) {
-            correctLetters.push(letter)
 
-            displayWord()
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter)
+
+                displayWord()
+            }
+            else {
+                showNotification()
+            }
         }
-        else {
-            showNotification()
-        }
-    }
-        else {
-            if (!wrongLetters.includes(Letter)) {
+        else if (!wrongLetters.includes(letter)) {
                 wrongLetters.push(letter)
 
                 updateWrongLettersEl()
-        }
+            }
             else {
                 showNotification()
-        }
+            }
+
     }
 })
 
 
+
+// Resart game and play again
 playAgainBtn.addEventListener('click', () => {
     correctLetters.length = 0
     wrongLetters.length = 0
+
     selectedIndex = Math.floor(word.length * Math.random())
     selectedWord = word[selectedIndex]
 
@@ -109,7 +118,9 @@ playAgainBtn.addEventListener('click', () => {
 
     updateWrongLettersEl()
 
-    popup.style.display = 'none'
-
+        popup.style.display = 'none'
 
 })
+
+
+displayWord()
